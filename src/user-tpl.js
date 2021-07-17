ad74ee0f04b35316d82e58089346040e1c72e4d0
tatidan <t.danyliuk@gmail.com>
2021-07-17 19:57:09 +0300

    project basics created

---
+++ src/user-tpl.js
@@ -0,0 +1,37 @@
+// import user from 'путь/к/user.json;
+
+// <Profile
+//   name={user.name}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />
+
+<div class="profile">
+  <div class="description">
+    <img
+      src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
+      alt="Аватар пользователя"
+      class="avatar"
+    />
+    <p class="name">Petra Marica</p>
+    <p class="tag">@pmarica</p>
+    <p class="location">Salvador, Brasil</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">1000</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">2000</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">3000</span>
+    </li>
+  </ul>
+</div>
